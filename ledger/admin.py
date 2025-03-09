dfc289503cf71a2c70e7b015d5cd4b4a77a60225
newonathe17 <ethanowen0117@gmail.com>
2025-03-10 01:17:52 +0800

    (For the Bonus) Made an admin panel for each of the models

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(RecipeIngredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    inlines = [RecipeIngredientInline,]
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Quantity', 'ingredient', 'recipe',)
+    search_fields = ('Quantity',)
+    list_filter = ('recipe', 'ingredient',)
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
