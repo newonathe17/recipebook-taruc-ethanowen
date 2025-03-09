5fe78747ee7cfb233b66362e340c5d4bce8a9a63
newonathe17 <ethanowen0117@gmail.com>
2025-03-09 20:08:09 +0800

    Defined models for Ingredient, Recipe, and RecipeIngredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient-detail', kwargs={'pk': self.pk})
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipe-detail', kwargs={'pk': self.pk})
+    
+class RecipeIngredient(models.Model):
+    Quantity = models.CharField(max_length=100)
+
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE, 
+        related_name="recipe")
+    
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE, 
+        related_name="ingredients")
+    
+    def __str__(self):
+        return f"{self.Quantity} of {self.ingredient.name}"
