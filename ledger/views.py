717d60159772bf2d1ab30447eab096feb65fe68d
newonathe17 <ethanowen0117@gmail.com>
2025-03-17 09:51:27 +0800

    restricted recipe views to logged-in users

--- ledger/views.py
+++ ledger/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipeList.html', {'recipes': recipes})
 
+@login_required
 def recipe_detail(request, pk):
-    recipe = get_object_or_404(Recipe, pk=pk)
+    recipes = Recipe.objects.filter(pk=pk)
+    if recipes.exists():
+        recipe = recipes.first()
+    else:
+        return render(request, 'recipeList.html', {'recipes': Recipe.objects.all(), 'error': 'Recipe not found.'})
     return render(request, 'recipe.html', {'recipe': recipe})
